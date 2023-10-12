808dbed693241eaaaa0e9ee96b43f7673274ff38
Elurojo <130256069+Elurojo@users.noreply.github.com>
2023-10-12 02:18:02 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+window.sr = ScrollReveal ();
+
+    sr.reveal('.hero-principal', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-100px'
+    });
+    sr.reveal('.hero-inferior-imagen', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+    sr.reveal('.seccion-texto', {
+        duration: 2000,
+        origin: 'left',
+        distance: '100px'
+    });
+    sr.reveal('.footer-logo', {
+        duration: 3000,
+        rotate: {
+            x:1,
+            y:180
+        }
+    });
+    sr.reveal('.experiencia-titulo"', {
+        duration: 2000,
+        origin: 'rigth',
+        distance: '100px'
+    });
